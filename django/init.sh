a2217c844f38b6bf5306b8efc3756114ef128158
Your Name <vuk.vasic@hes-so.ch>
2025-05-30 14:05:20 +0200

    Debuging Project

--- django/init.sh
+++ django/init.sh
@@ -1,26 +1,28 @@
-#! /bin/bash
+#!/bin/bash
 
+cd /code
+
+# Set Django superuser credentials
 export DJANGO_SUPERUSER_EMAIL=admin@admin.com
 export DJANGO_SUPERUSER_USERNAME=admin
 export DJANGO_SUPERUSER_PASSWORD=admin
 
-  python geoweb/manage.py makemigrations admin
-  python geoweb/manage.py migrate admin 
-  python geoweb/manage.py migrate auth 
-  python geoweb/manage.py migrate contenttypes 
-  python geoweb/manage.py migrate sessions 
+# Run migrations for Django system apps
+python geoweb/manage.py makemigrations admin
+python geoweb/manage.py migrate admin 
+python geoweb/manage.py migrate auth 
+python geoweb/manage.py migrate contenttypes 
+python geoweb/manage.py migrate sessions 
 
+# Create superuser and import geodata if successful
 if python geoweb/manage.py createsuperuser --noinput ; then
-   
-  echo "we export"
-  export PGPASSWORD='admin' 
-  shp2pgsql -I -s 4326:4326 cantons/cantons.shp cantons | psql -h db -p 5432 -d postgres -U postgres 
-
-  python geoweb/manage.py createsuperuser --noinput
-  python geoweb/manage.py migrate swissgeo --fake
+   echo "Creating superuser and importing geodata..."
+   export PGPASSWORD='admin' 
+   shp2pgsql -I -s 4326:4326 cantons/cantons.shp cantons | psql -h db -p 5432 -d postgres -U postgres 
+   python geoweb/manage.py migrate swissgeo --fake
 else 
-  echo "no export"
-
+   echo "Superuser creation failed"
 fi
 
+# Start Django development server
 python geoweb/manage.py runserver 0.0.0.0:8000
